6001f4a897b30bf3eaaba89061d867d99b87a056
Phil <walkerp07@yahoo.com>
2024-11-01 21:41:27 -0700

    Debug of contract on forked net using Debugger Pro

--- contracts-dbg/ArbitrageBot/ArbitrageBot.t.sol
+++ contracts-dbg/ArbitrageBot/ArbitrageBot.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.4.21 < 0.9.0;
 
-import "../../contracts/Lock.sol";
+import "../../contracts/ArbitrageBot.sol";
 
 contract DbgEntry {
     // --------------------------------------------- To fake fund wallet and move block
@@ -11,24 +11,27 @@ contract DbgEntry {
     event EvmPrint(address);
     event EvmPrint(uint256);
     // ----------------------------------------------
+
+    address pool = 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e;
     constructor() {
         emit EvmPrint("DbgEntry.constructor");
 
         // ----------- START CODE ----------------
         emit EvmSetBalance(address(this), 1000);
-        Lock lock = new Lock{value: 100}(block.timestamp + 100);
+        ArbitrageBot bot = new ArbitrageBot(pool);
 
         emit EvmSetBlockTimestamp(block.timestamp + 101);
 
         emit EvmPrint("Balance before:");
         emit EvmPrint(address(this).balance);
 
-        lock.withdraw();
+        bot.getNewString();
 
         emit EvmPrint("Balance after:");
         emit EvmPrint(address(this).balance);
         
         // ----------- END ----------------
+
         emit EvmPrint("DbgEntry return");
     }
 }
